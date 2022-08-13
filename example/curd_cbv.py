10d560b1cbc6ec5d6c46b213c9a05a74bd9806c8
jjj <wszsdpyjjj@gmail.com>
2022-08-13 15:19:42 +0800

    polish cbv example

--- example/curd_cbv.py
+++ example/curd_cbv.py
@@ -1,20 +1,20 @@
-# cbv
+# 定义cbv类型视图
 
-from simple_starlette import SimpleStarlette, Request
 from simple_starlette.args import QueryParams
+from simple_starlette import SimpleStarlette, Request
 from simple_starlette.responses import Response, ResTypeEnum
 
 app = SimpleStarlette(__name__)
 
-# 推荐使用这种方式 
 @app.route("/index")
 class Index:
     class IndexQuery(QueryParams):
         a: int
         b: int
 
-    async def get(self, request: Request, q: IndexQuery): # 定义 get 方法
+    # 定义 get 方法
+    async def get(self, request: Request, q: IndexQuery):
         return Response({"a": q.a, "b": q.b}, ResTypeEnum.JSON)
     
-
-app.run()
+if __name__ == "__main__":
+    app.run()
