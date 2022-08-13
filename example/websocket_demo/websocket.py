35408eea6c38ce2f6fa31d9765db94bf09a7ec4d
jjj <wszsdpyjjj@gmail.com>
2022-08-13 15:04:33 +0800

    websocket demo

--- example/websocket_demo/websocket.py
+++ example/websocket_demo/websocket.py
@@ -9,14 +9,15 @@ from simple_starlette import SimpleStarlette
 
 app = SimpleStarlette(__name__)
 
-
 @app.route("/ws", websocket_route=True)
 async def test(websocket: WebSocket):
     await websocket.accept()
+    print(websocket.cookies)
     while True:
         data = await websocket.receive_text()
-        if data == "ok":
+        if data == "close_conn":
             break
         await websocket.send_text(f"xxx: {data}")
 
-app.run()
+if __name__ == "__main__":
+    app.run()
