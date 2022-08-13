9d37e0afa01a175539414bce9796ff4d7a702165
jjj <wszsdpyjjj@gmail.com>
2022-08-13 15:19:23 +0800

    polish token_auth middleware example

--- example/auth_middleware.py
+++ example/auth_middleware.py
@@ -1,5 +1,5 @@
-# TokenAuthMiddleWareGenFunc 中间件使用示例
 # 标准jwt token
+# TokenAuthMiddleWareGenFunc , Cors 中间件使用示例
 
 from starlette.requests import Request
 from starlette.responses import JSONResponse
@@ -7,6 +7,7 @@ from starlette.responses import JSONResponse
 from simple_starlette import SimpleStarlette
 from simple_starlette.middleware.token_auth import (TokenAuthMiddleWareGenFunc,
                                                     register_skip_auth_routes)
+from simple_starlette.middleware.cors import CorsMiddlewareGenFunc
 
 
 async def check_is_login_process(payload: dict) -> bool:
@@ -26,32 +27,40 @@ app = SimpleStarlette(
         TokenAuthMiddleWareGenFunc(
             validate_process=check_is_login_process,
             on_error=custom_on_error,
+            expires=60 * 60,
+            httponly=True
+        ),
+        CorsMiddlewareGenFunc(
+            allow_origins=["http://127.0.0.1:5500"],
+            allow_credentials=True,
+            allow_methods=("GET","POST",)
         )
     ],
 )
 
-@app.route("/need_login_api")
+@app.route("/need_login_api", allow_methods=["GET"])
 async def ping(request: Request):
-    return JSONResponse({"ping": "pong"})
+    _ = request.auth.get("test_is_login")
+    headers={"Access-Control-Allow-Credentials": "true"}
+    return JSONResponse({"ping": "pong"},headers=headers)
 
 
-@app.route("/set_session_api")
+@app.route("/login", allow_methods=["GET"])
 async def set_session_api(request: Request):
     # 登录模拟
     # 使用 request.auth 操作 session
     request.auth["test_is_login"] = True
-    return JSONResponse({"is_success": True})
+    return JSONResponse({"is_login": True})
 
-
-@app.route("/clear_session_api")
+@app.route("/logout", allow_methods=["GET"])
 async def clear_session_api(request: Request):
     # 退出登录
     del request.auth["test_is_login"]
-    return JSONResponse({"is_success": True})
-
+    return JSONResponse({"is_logout": True})
 
 register_skip_auth_routes(
     [set_session_api, clear_session_api]
 )  #  排除一些不需要登陆的接口
 
-app.run()
+if __name__ == "__main__":
+    app.run(debug=True, host="127.0.0.1", port=9091)
