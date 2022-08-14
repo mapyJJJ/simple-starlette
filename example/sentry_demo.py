f113db8282b68c8c9de9a4069918fb528ea7d285
jjj <wszsdpyjjj@gmail.com>
2022-08-14 22:00:27 +0800

    sentry demo

---
+++ example/sentry_demo.py
@@ -0,0 +1,19 @@
+from simple_starlette import Request
+from sentry_sdk.integrations.asgi import SentryAsgiMiddleware
+from starlette.middleware import Middleware
+from simple_starlette import SimpleStarlette
+import sentry_sdk
+
+sentry_sdk.init(dsn="xxxxxxxxxxxxxxxxxx")  # 此处放入sentry dsn地址
+app = SimpleStarlette(
+    __name__, middleware=[Middleware(SentryAsgiMiddleware)]
+)
+
+
+@app.route("/raise_error", allow_methods=["GET"])
+async def raise_error(request: Request):
+    1 / 0
+
+
+if __name__ == "__main__":
+    app.run()
