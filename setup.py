03ed3435214c5449c704b0c3d940fe825574b720
jjj <wszsdpyjjj@gmail.com>
2023-02-11 22:54:36 +0800

    update setup

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 import os
 from setuptools import find_packages, setup
 
-__version__ = "2.2.1"
+__version__ = "2.3.0"
 
 find_file_path = (
     lambda file_name: os.path.split(__file__)[0] + "/" + file_name
 )
 
 requires = (
-    open(find_file_path("dev_requirements.txt"), "r")
+    open(find_file_path(os.path.join(os.getcwd(),"dev_requirements.txt")), "r")
     .read()
     .split("/n")
 )
-with open(find_file_path("README.md"), "r", encoding="utf-8") as fh:
+with open(find_file_path(os.path.join(os.getcwd(), "README.md")), "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
