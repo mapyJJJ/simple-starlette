6b75db677eb2fa0e88714779af24383543c0cf44
jjj <wszsdpyjjj@gmail.com>
2023-02-12 16:27:57 +0800

    fix setup

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ find_file_path = (
 )
 
 requires = (
-    open(find_file_path(os.path.join(os.getcwd(),"dev_requirements.txt")), "r")
+    open(find_file_path("dev_requirements.txt"), "r")
     .read()
     .split("/n")
 )
-with open(find_file_path(os.path.join(os.getcwd(), "README.md")), "r", encoding="utf-8") as fh:
+with open(find_file_path("README.md"), "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
